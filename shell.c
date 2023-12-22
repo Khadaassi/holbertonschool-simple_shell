f77e25740374428b1840c1072c102ed6dcb063c2
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2023-12-22 09:28:03 +0000

    fixing code to fit betty coding style

--- shell.c
+++ shell.c
@@ -12,38 +12,38 @@ int main(int argc, char **argv)
 	size_t len = 0;
 	ssize_t read_chars;
 
-    /*Check if arguments count is not equal to 1*/
+	/*Check if arguments count is not equal to 1*/
 	if (argc != 1)
 	{
 		printf("Usage: %s\n", argv[0]);
 		return (EXIT_FAILURE); // Return failure
 	}
-    /*Loop to continuously read input*/ 
+	/*Loop to continuously read input*/ 
 	while (1)
 	{
-        /*Display the shell prompt if input is from terminal*/
+		/*Display the shell prompt if input is from terminal*/
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
-        /*Read input from stdin*/
+		/*Read input from stdin*/
 		read_chars = getline(&input, &len, stdin);
-        /*If end of file or an error occurs, break out of loop*/
+		/*If end of file or an error occurs, break out of loop*/
 		if (read_chars == -1)
 			break;
-        /*If input is only whitespace, continue*/
+		/*If input is only whitespace, continue*/
 		cleaned_input = remove_whitespace(input);
 		if (cleaned_input == NULL)
 			continue;
-        /*If input is exit, break out of loop*/
+		/*If input is exit, break out of loop*/
 		if (strcmp(cleaned_input, "exit\n") == 0)
 			break;
-        /*Execute the command and store the result*/
+		/*Execute the command and store the result*/
 		execution_result = execute_command(cleaned_input);
 
-        /*If execution result is -1, print error*/
-        if (execution_result == -1)
+		/*If execution result is -1, print error*/
+		if (execution_result == -1)
 			perror(argv[0]);
 	}
-    /*Free allocated memory*/
+	/*Free allocated memory*/
 	free(input);
 	return (0); // Return success
 }
--- splitter.c
+++ splitter.c
@@ -10,28 +10,28 @@ char **splitter(char *input)
 	char **arguments = NULL;
 	int arg_count = 0;
 
-    /*Allocate memory for the arguments array*/
+	/*Allocate memory for the arguments array*/
 	arguments = calloc(10, sizeof(char *));
 	if (arguments == NULL)
 	{
 		perror("Memory allocation failed");
 		exit(1);
 	}
-    /*Tokenize the input string using strtok*/
+	/*Tokenize the input string using strtok*/
 	token = strtok(input, " \t\n");
 	if (token == NULL)
 	{
 		free(arguments);
 		return (NULL);
 	}
-    /*Loop through the tokens and store them in the arguments array*/
+	/*Loop through the tokens and store them in the arguments array*/
 	while (token != NULL)
 	{
 		arguments[arg_count++] = token;
 		token = strtok(NULL, " \t\n");
 	}
 
-    /*Set the last element of the arguments array to NULL*/
+	/*Set the last element of the arguments array to NULL*/
 	arguments[arg_count] = NULL;
 	return (arguments);
 }
