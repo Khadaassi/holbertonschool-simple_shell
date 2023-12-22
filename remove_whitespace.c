3dd791391d9cd1d458ffffc1fd1a6a6001b66450
Khadaassi <aassikhadija@gmail.com>
2023-12-22 10:06:24 +0100

    Fix betty in remove_whitespace function

--- remove_whitespace.c
+++ remove_whitespace.c
@@ -7,15 +7,15 @@
  */
 char *remove_whitespace(char *input)
 {
-    char *cleaned_input = input;
+	char *cleaned_input = input;
 
 /* Remove leading whitespace */
-    while (*cleaned_input == ' ' || *cleaned_input == '\t') 
-        cleaned_input++;
+	while (*cleaned_input == ' ' || *cleaned_input == '\t')
+		cleaned_input++;
 
 /* Remove trailing whitespace */
-    if (*cleaned_input == '\n' || *cleaned_input == '\0')
-        return (NULL);
+	if (*cleaned_input == '\n' || *cleaned_input == '\0')
+		return (NULL);
 
-    return (cleaned_input);
+	return (cleaned_input);
 }
