7a59e984cfa4fd8c8c5572310e7d20d7e43a23d6
Nathan9722 <146355716+Nathan9722@users.noreply.github.com>
2023-12-22 09:08:34 +0000

    shell.c: Clean up code and add comments

--- shell.c
+++ shell.c
@@ -11,27 +11,39 @@ int main(int argc, char **argv)
 	int execution_result;
 	size_t len = 0;
 	ssize_t read_chars;
+
+    /*Check if arguments count is not equal to 1*/
 	if (argc != 1)
 	{
 		printf("Usage: %s\n", argv[0]);
-		return (EXIT_FAILURE);
+		return (EXIT_FAILURE); // Return failure
 	}
+    /*Loop to continuously read input*/ 
 	while (1)
 	{
+        /*Display the shell prompt if input is from terminal*/
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
+        /*Read input from stdin*/
 		read_chars = getline(&input, &len, stdin);
+        /*If end of file or an error occurs, break out of loop*/
 		if (read_chars == -1)
 			break;
+        /*If input is only whitespace, continue*/
 		cleaned_input = remove_whitespace(input);
 		if (cleaned_input == NULL)
 			continue;
+        /*If input is exit, break out of loop*/
 		if (strcmp(cleaned_input, "exit\n") == 0)
 			break;
+        /*Execute the command and store the result*/
 		execution_result = execute_command(cleaned_input);
-		if (execution_result == -1)
+
+        /*If execution result is -1, print error*/
+        if (execution_result == -1)
 			perror(argv[0]);
 	}
+    /*Free allocated memory*/
 	free(input);
-	return (0);
+	return (0); // Return success
 }
