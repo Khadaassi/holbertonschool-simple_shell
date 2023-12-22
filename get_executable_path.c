a7d0b6349718f091a1d59b2bd4a66bd47c312f83
Khadaassi <aassikhadija@gmail.com>
2023-12-22 10:01:58 +0100

    Refactor and add comment get_executable_path function

--- get_executable_path.c
+++ get_executable_path.c
@@ -8,34 +8,34 @@
  */
 char *get_executable_path(char *executable_name)
 {
-	char *path_env; /* PATH environment variable */
-	char *path_copy; /* Copy of PATH environment variable */
-	char *path_token; /* Tokenized path */
-	char *result; /* Full path of executable */
-	char full_path[1024]; /* Full path of executable */
+    char *path_env; /* PATH environment variable */
+    char *path_copy; /* Copy of PATH environment variable */
+    char *path_token; /* Tokenized path */
+    char *result; /* Full path of executable */
+    char full_path[1024]; /* Full path of executable */
 
-	path_env = getenv("PATH"); /* Get PATH environment variable */
+    path_env = getenv("PATH"); /* Get PATH environment variable */
 
-	if (path_env == NULL) /* If PATH is not set */
-		return (NULL);
+    if (path_env == NULL) /* If PATH is not set */
+        return (NULL);
 
-	path_copy = strdup(path_env); /* Make a copy of PATH */
-	path_token = strtok(path_copy, ":"); /* Tokenize PATH */
+    path_copy = strdup(path_env); /* Make a copy of PATH */
+    path_token = strtok(path_copy, ":"); /* Tokenize PATH */
 
-	while (path_token) /* Loop through tokens */
-	{
-		/* Build full path */
-		snprintf(full_path, sizeof(full_path), "%s/%s", path_token, executable_name);
+    while (path_token) /* Loop through tokens */
+    {
+        /* Build full path */
+        snprintf(full_path, sizeof(full_path), "%s/%s", path_token, executable_name);
 
-		if (access(full_path, F_OK | X_OK) == 0)
-		{
-			result = strdup(full_path); /* Make a copy of full path */
-			free(path_copy); /* Free copy of PATH */
-			return (result);
-		}
-		path_token = strtok(NULL, ":"); /* Get next token */
-	}
-	free(path_copy);
+        if (access(full_path, F_OK | X_OK) == 0)
+        {
+            result = strdup(full_path); /* Make a copy of full path */
+            free(path_copy); /* Free copy of PATH */
+            return (result);
+        }
+        path_token = strtok(NULL, ":"); /* Get next token */
+    }
+    free(path_copy);
 
-	return (NULL);
+    return (NULL);
 }
